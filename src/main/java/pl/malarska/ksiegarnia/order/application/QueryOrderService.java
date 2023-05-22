64ea36f7ec821a506cff4e9ac6a2bd97c82609da
zelek <mzelechowski79@gmail.com>
2023-05-22 17:43:22 +0200

    JPA - oneToMany

--- src/main/java/pl/malarska/ksiegarnia/order/application/QueryOrderService.java
+++ src/main/java/pl/malarska/ksiegarnia/order/application/QueryOrderService.java
@@ -3,8 +3,8 @@ package pl.malarska.ksiegarnia.order.application;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.malarska.ksiegarnia.order.application.port.QueryOrderUseCase;
+import pl.malarska.ksiegarnia.order.db.OrderJpaRepository;
 import pl.malarska.ksiegarnia.order.domain.Order;
-import pl.malarska.ksiegarnia.order.domain.OrderRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class QueryOrderService implements QueryOrderUseCase {
-    private final OrderRepository repository;
+    private final OrderJpaRepository repository;
 
     public List<Order> findAll() {
         return repository.findAll();
